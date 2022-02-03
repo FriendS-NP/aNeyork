da24636c82f06854ab842c9c4b21181ba210182e
Neyork <94509835+Neyork@users.noreply.github.com>
2022-02-03 17:10:44 +0200

    Add files via upload

---
+++ Neyork/modules/alive.py
@@ -0,0 +1,24 @@
+import os
+import re
+from platform import python_version as kontol
+from telethon import events, Button
+from telegram import __version__ as telever
+from telethon import __version__ as tlhver
+from pyrogram import __version__ as pyrover
+from Neyork.events import register
+from Neyork import telethn as tbot
+
+
+PHOTO = "https://telegra.ph/file/d6668ed2144be07b180df.jpg"
+
+@register(pattern=("/alive"))
+async def awake(event):
+  TEXT = f"**Hi [{event.sender.first_name}](tg://user?id={event.sender.id}), I'm Neyork Robot.** \n\n"
+  TEXT += "💠 **I'm Working Properly** \n\n"
+  TEXT += f"💠 **My Master : [MohammeD](https://t.me/QDevQ)** \n\n"
+  TEXT += f"💠 **Library Version :** `{telever}` \n\n"
+  TEXT += f"💠 **Telethon Version :** `{tlhver}` \n\n"
+  TEXT += f"💠 **Pyrogram Version :** `{pyrover}` \n\n"
+  TEXT += "**Thanks For Adding Me Here ❤️**"
+  BUTTON = [[Button.url("Help", "https://t.me/Source1bot?start=help"), Button.url("Support", "https://t.me/NeyorkSupport")]]
+  await tbot.send_file(event.chat_id, PHOTO, caption=TEXT,  buttons=BUTTON)
